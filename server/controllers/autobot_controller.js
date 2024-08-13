b20c54f570ce50da0ea9e22805aa7a3ae3f4561e
arizvi-prog <alishajrizvi@gmail.com>
2024-08-13 14:38:50 -0400

    added more to routes and models

--- server/controllers/autobot_controller.js
+++ server/controllers/autobot_controller.js
@@ -1,18 +1,24 @@
-const Autobot = require('../models/Autobot');
+const { Autobot, User } = require('../models');
+
 
 module.exports = {
     async getAllAutobots(req, res) {
+        console.log(req.cookies);
         const autobots = await Autobot.find();
-    
+
         res.json(autobots);
     },
 
     async createAutobot(req, res) {
+        const user = await User.findById(req.user_id);
         // Create the autobot
         const newAutobot = await Autobot.create({
             name: req.body.name,
             color: req.body.color
         });
+
+        user.autobots.push(newAutobot._id);
+        await user.save();
 
         res.json({
             message: 'Autobot created successfully!',
